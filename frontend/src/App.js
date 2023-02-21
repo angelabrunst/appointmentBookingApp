d24e6f15268712c15d40b10f891e91b169b4a570
angela.brunst <angela.hairstylist@gmail.com>
2023-02-20 20:50:04 -0700

    Added Frontend - Errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
 
 import './App.css';
 
 class App extends Component {
     render() {
-        return ( <div className='App' >
-
-            </div>
+        return ( 
+            <BrowserRouter>
+                <Switch>
+                    <Redirect from='/' to='/auth' exact />
+                    <Route path='/auth' Component={AuthPage} />
+                    <Route path='/events' Component={null} />
+                    <Route path='/booking' Component={null} />
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
