1fb504c41fadcd032eb425738c8bd72f71974675
angela.brunst <angela.hairstylist@gmail.com>
2023-02-26 11:41:47 -0700

    The routing finally works! Insert evil laugh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,49 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Navigate, Routes } from 'react-router-dom';
 
 import AuthPage from './pages/Auth';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
 
 import './App.css';
 
 class App extends Component {
-    render() {
-        return ( 
-            <BrowserRouter>
-                <Switch>
-                    <Redirect from='/' to='/auth' exact />
-                    <Route path='/auth' Component={AuthPage} />
-                    <Route path='/events' Component={null} />
-                    <Route path='/booking' Component={null} />
-                </Switch>
-            </BrowserRouter>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/auth" />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/bookings" element={<BookingsPage />} />
+        </Routes>
+      </BrowserRouter>
+    )
+  }
 }
 
 
 export default App;
+// import React from 'react';
+// import { BrowserRouter, Route, Navigate, Routes } from 'react-router-dom';
+
+// import Auth from './pages/Auth';
+// import Bookings from './pages/Bookings';
+// import Events from './pages/Events';
+
+// import './App.css';
+
+// function App() {
+//     return (
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Navigate replace to="/auth" />} />
+//           <Route path="/auth" element={<Auth />} />
+//           <Route path="/events" element={<Events />} />
+//           <Route path="/bookings" element={<Bookings />} />
+//         </Routes>
+//       </BrowserRouter>
+//     );
+// }
+
+// export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
+// import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-// ReactDOM.render(<App /> , document.getElementById('root'));
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+ReactDOM.render(<App /> , document.getElementById('root'));
